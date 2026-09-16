b9359ced3f330d148cc7fa6addc27db3a7ac92db
unknown <alisaselviasaharani@gmail.com>
2026-09-16 14:35:29 +0700

    Tutorial 03: Form dan Data Delivery

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_home, show_aboutme, show_experience, show_education
+from main.views import show_home, show_aboutme, show_experience, show_education, show_projects, create_project, get_projects_json,delete_project
 
 # Memberikan namespace "main" untuk URL pada aplikasi main.
 app_name = "main"
@@ -17,4 +17,13 @@ urlpatterns = [
     
     # URL halaman Education.
     path("education/", show_education, name="show_education"),
+    
+    path("projects/", show_projects, name="show_projects"),
+
+    path("projects/add/", create_project, name="create_project"),
+
+
+    path("api/projects/", get_projects_json, name="get_projects_json"),
+
+    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
 ]
